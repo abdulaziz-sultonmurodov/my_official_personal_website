47da7a134327366879817142f743bfa42fcd820a
Jonny221201 <abdulaziz221201@gmail.com>
2021-04-20 02:04:55 +0500

    ready

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -39,7 +39,7 @@ const ContactPage = () => {
             <h1 className="uppercase text-lg text-center sm:text-2xl lg:text-3xl text-darkGrey">
               Have some questions?
             </h1>
-            <p className="mt-3 tracking-wider text-center text-sm md:text-md flex items-center justify-center text-grey">
+            <p className="mt-3 tracking-wider text-center text-xs md:text-md flex items-center justify-center text-grey">
               <IoLocationSharp className="mr-2 w-5 h-5 text-grey" />
               Uchtepa Region, Tashkent City, Uzbekistan
             </p>
@@ -48,11 +48,27 @@ const ContactPage = () => {
             <div className="hidden lg:flex">
               <RiMailSendLine className="w-80 h-80 text-grey hover:text-darkGrey cursor-pointer" />
             </div>
-            <form className="flex flex-col lg:ml-48">
-              <input className="bg-gray-300 py-2 px-4 rounded-2xl focus:outline-none focus:ring-2 focus:bg-white focus:ring-gray-500 mt-4" type="text" placeholder="Your Name" />
-              <input className="bg-gray-300 py-2 px-4 rounded-2xl focus:outline-none focus:ring-2 focus:bg-white focus:ring-gray-500 mt-4" type="email" placeholder="Your Email" />
-              <textarea className="bg-gray-300 py-2 px-4 rounded-2xl focus:outline-none focus:ring-2 focus:bg-white  focus:ring-gray-500 mt-4" cols="30" rows="5" placeholder="Your Questions..."></textarea>
-              <input className="mt-4 rounded-2xl bg-grey text-white py-2 w-1/2 mx-auto hover:bg-darkGrey cursor-pointer" type="submit" />
+            <form className="flex flex-col w-64 lg:ml-48">
+              <input
+                className="bg-gray-300 py-2 px-4 rounded-2xl focus:outline-none focus:ring-2 focus:bg-white focus:ring-gray-500 mt-4"
+                type="text"
+                placeholder="Your Name"
+              />
+              <input
+                className="bg-gray-300 py-2 px-4 rounded-2xl focus:outline-none focus:ring-2 focus:bg-white focus:ring-gray-500 mt-4"
+                type="email"
+                placeholder="Your Email"
+              />
+              <textarea
+                className="bg-gray-300 py-2 px-4 rounded-2xl focus:outline-none focus:ring-2 focus:bg-white  focus:ring-gray-500 mt-4"
+                cols="30"
+                rows="5"
+                placeholder="Your Questions..."
+              ></textarea>
+              <input
+                className="mt-4 rounded-2xl bg-grey text-white py-2 w-1/2 mx-auto hover:bg-darkGrey cursor-pointer"
+                type="submit"
+              />
             </form>
           </div>
         </div>
